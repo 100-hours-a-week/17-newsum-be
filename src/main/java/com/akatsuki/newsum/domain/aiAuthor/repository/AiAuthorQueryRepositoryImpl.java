cd45d4f3c62cb9aa2a6312e35b7e0a4ae37289e1
gongsua <91416981+gongsua@users.noreply.github.com>
2025-06-16 15:44:03 +0900

    [Feat] #216 유저 AI 작가 구독 API 토글 구현 (#219)
    * feat: entity, flyway 생성
    
    * feat : service, controller 작가 구독 토글
    
    * feat : user 인증 예외 처리 수정
    
    * refactor : MapTOcardDto 엔티티에 의존하지 않도록 수정
    
    * feat: 작가 상세페이지 정보 dto 생성
    
    * feat: ai 작가 상세 페이지 정보 구현
    
    * feat: 커서기반 작가 페이지 구현
    
    * feat : ai_author 테이블 생성시각 추가
    
    * feat : ai_author 테이블 생성시각 추가
    
    * feat : created at 기준으로 커서 정렬 하도록 수정
    
    * feat: 작가 리스트에서 구독한 상태인지 아닌지 확인 추가
    
    * feat: 상세페이지에서 구독 상태 확인 추가
    
    * refactor : af에서 fav 로 네이밍 변경
    
    * refactor : 성공 응답 메세지 AI_AUTHOR_SUBSCRIBE_TOGGLE_SUCCESS 로 변경
    
    * refactor: 작가 목록 리스트를 가나다순으로 정렬
    
    * refactor: queryDSL 의 static 인스턴스 필드 사용
    
    * remove : author 테이블에 created at 삭제

---
+++ src/main/java/com/akatsuki/newsum/domain/aiAuthor/repository/AiAuthorQueryRepositoryImpl.java
@@ -0,0 +1,60 @@
+package com.akatsuki.newsum.domain.aiAuthor.repository;
+
+import static com.akatsuki.newsum.domain.aiAuthor.entity.QAiAuthor.*;
+import static com.akatsuki.newsum.domain.user.entity.QAuthorFavorite.*;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Repository;
+
+import com.akatsuki.newsum.common.pagination.model.cursor.Cursor;
+import com.akatsuki.newsum.common.pagination.model.query.QueryEngineType;
+import com.akatsuki.newsum.common.pagination.model.query.QueryFragment;
+import com.akatsuki.newsum.common.pagination.query.registry.CursorPageQueryRegistry;
+import com.akatsuki.newsum.domain.aiAuthor.entity.AiAuthor;
+import com.akatsuki.newsum.domain.aiAuthor.entity.QAiAuthor;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class AiAuthorQueryRepositoryImpl implements AiAuthorQueryRepository {
+
+	private final JPAQueryFactory queryFactory;
+	private final CursorPageQueryRegistry cursorPageQueryRegistry;
+
+	@Override
+	public List<AiAuthor> findByCursorAndSize(Cursor cursor, int size) {
+		QueryFragment fragment = cursorPageQueryRegistry.resolve(
+			QAiAuthor.class,
+			cursor,
+			QueryEngineType.QUERYDSL
+		).buildQueryFragment(cursor);
+
+		return queryFactory
+			.selectFrom(aiAuthor)
+			.where((Predicate)fragment.whereClause())
+			.orderBy((OrderSpecifier<?>[])fragment.orderByClause())
+			.limit(size + 1)
+			.fetch();
+	}
+
+	@Override
+	public Set<Long> findSubscribedAuthorIdsByUserId(Long userId, List<Long> aiAuthorIds) {
+		return queryFactory
+			.select(authorFavorite.aiAuthor.id)
+			.from(authorFavorite)
+			.where(
+				authorFavorite.userId.eq(userId),
+				authorFavorite.aiAuthor.id.in(aiAuthorIds)
+			)
+			.fetch()
+			.stream()
+			.collect(Collectors.toSet());
+	}
+}
