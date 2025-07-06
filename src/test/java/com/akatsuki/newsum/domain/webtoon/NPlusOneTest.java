3518ae394765c64da214f1da7945e98d7641882d
gongsua <91416981+gongsua@users.noreply.github.com>
2025-07-07 01:00:30 +0900

    chore : yml databases 업데이트 none 으로 수정

--- src/test/java/com/akatsuki/newsum/domain/webtoon/NPlusOneTest.java
+++ src/test/java/com/akatsuki/newsum/domain/webtoon/NPlusOneTest.java
@@ -2,13 +2,13 @@ package com.akatsuki.newsum.domain.webtoon;
 
 import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.akatsuki.newsum.domain.webtoon.entity.webtoon.Category;
-import com.akatsuki.newsum.domain.webtoon.entity.webtoon.Webtoon;
-import com.akatsuki.newsum.domain.webtoon.repository.WebtoonRepository;
+import com.akatsuki.newsum.domain.webtoon.entity.webtoon.WebtoonLike;
+import com.akatsuki.newsum.domain.webtoon.repository.WebtoonLikeRepository;
 
 import jakarta.transaction.Transactional;
 
@@ -18,13 +18,17 @@ import jakarta.transaction.Transactional;
 class NPlusOneTest {
 
 	@Autowired
-	WebtoonRepository webtoonRepository;
+	WebtoonLikeRepository webtoonLikeRepository;
 
 	@Test
-	void nPlusOne_확인() {
-		List<Webtoon> webtoons = webtoonRepository.findWebtoonByCategory(Category.IT);
-		for (Webtoon webtoon : webtoons) {
-			System.out.println(webtoon.getAiAuthor().getName());
+	@DisplayName("웹툰좋아요에 대한 N+1 가져오기 ")
+	void webtoonLike_NPlusOne_테스트() {
+		List<WebtoonLike> likes = webtoonLikeRepository.findAll();
+
+		for (WebtoonLike like : likes) {
+			// 여기서 Lazy 로딩 트리거!
+			// String title = like.getWebtoon().getTitle();
+			System.out.println("웹툰 제목 = " + like.getWebtoon().getCreatedAt());
 		}
 	}
 }
